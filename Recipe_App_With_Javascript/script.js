f48f0c98dec793d32a22e9cb05bb997f97f20a89
ElHakikAmina <amina.el.hakik2@gmail.com>
2023-01-29 20:58:38 +0100

    +

---
+++ Recipe_App_With_Javascript/script.js
@@ -0,0 +1,46 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+let userInp = document.getElementById("user-inp").value;
+
+fetch(url+"cake")
+.then((response)=>response.json())
+.then((data) =>{
+let myMeal = data.meals[0];
+
+let count = 1;
+let ingredients = [];
+for(let i in myMeal){
+    let ingredient ="";
+    let measure = "";
+    if (i.startsWith("strIngredient") && myMeal[i]){
+        ingredient = myMeal[i];
+        measure = myMeal[`strMeasure`+count];
+        count +=1;
+        ingredients.push(`${measure} ${ingredient}`);
+    }
+}
+
+
+result.innerHTML=`
+<img src=${myMeal.strMealThumb} >
+<div class="details">
+    <h2>${myMeal.strMeal}</h2>
+    <h4>${myMeal.strArea}</h4>
+</div> 
+<div id="ingredient-con"></div>
+<div id="recipe">
+    <button id="hide-recipe">X</button>
+    <pre id="instructions">
+        ${myMeal.strInstructions}
+    </pre>
+</div>
+<button id="show-recipe">View Recipe</button>
+`;
+let ingredientCon = document.getElementById("ingredient-con");
+let parent = document.createElement("ul");
+let recipe = document.getElementById("recipe");
+let hideRecipe = document.getElementById("hide-recipe");
+let showRecipe = document.getElementById("show-recipe");
+});
